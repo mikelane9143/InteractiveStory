4b2b03f233064b50b1c4faa62607dbde5983196b
Mike L <mikelane9143@gmail.com>
2015-06-11 22:23:02 +1000

    Next Steps - nearly working
    More onroad progress

--- app/src/main/java/com/mikelane9143/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/mikelane9143/interactivestory/ui/StoryActivity.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,10 +20,12 @@ public class StoryActivity extends ActionBarActivity {
     public static final String TAG = StoryActivity.class.getSimpleName();
 
     private Story mStory = new Story();
+    private Page mCurrentPage;
     private ImageView mImageView;
     private TextView mTextView;
     private Button mChoice1;
     private Button mChoice2;
+    private String mName;
 
 
     @Override
@@ -31,28 +34,49 @@ public class StoryActivity extends ActionBarActivity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        mName = intent.getStringExtra(getString(R.string.key_name));
 
-        if (name == null) {
-            name = "Mike";
+        if (mName == null) {
+            mName = "Mike";
         }
-        Log.d(TAG, name);
+        Log.d(TAG, mName);
 
         mImageView = (ImageView)findViewById(R.id.storyImageView);
         mTextView = (TextView)findViewById(R.id.storyTextView);
         mChoice1 = (Button)findViewById(R.id.choiceButton1);
         mChoice2 = (Button)findViewById(R.id.choiceButton2);
+
+        loadPage(0);
     }
 
-    private void loadPage() {
-        Page page = mStory.getPage(0);
+    private void loadPage(int choice) {
+        mCurrentPage = mStory.getPage(choice);
 
-        Drawable drawable = getResources().getDrawable(page.getImageId());
+        Drawable drawable = getResources().getDrawable(mCurrentPage.getImageId());
         mImageView.setImageDrawable(drawable);
 
-        mTextView.setText(page.getText());
+        String pageText = mCurrentPage.getText();
+        //Add the name if placeholder included, otherwise not
+        pageText =String.format(pageText, mName);
+        mTextView.setText(pageText);
 
-        mChoice1.setText(page.getChoice1().getText());
-        mChoice2.setText(page.getChoice2().getText());
+        mChoice1.setText(mCurrentPage.getChoice1().getText());
+        mChoice2.setText(mCurrentPage.getChoice2().getText());
+
+        mChoice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nextPage = mCurrentPage.getChoice1().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+
+        mChoice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nextPage = mCurrentPage.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
     }
 }
